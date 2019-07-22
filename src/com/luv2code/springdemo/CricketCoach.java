a3ea62805527520e707211620b4d9361d9e8a18b
youngminshin <youngminshin@192.168.10.9>
2019-07-22 20:56:17 +0900

    Injecting literal values

--- src/com/luv2code/springdemo/CricketCoach.java
+++ src/com/luv2code/springdemo/CricketCoach.java
@@ -3,6 +3,24 @@ package com.luv2code.springdemo;
 public class CricketCoach implements Coach {
 
 	private FortuneService fortuneService;
+	private String emailAddress;
+	private String team;
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		this.team = team;
+	}
 
 	public CricketCoach() {
 		System.out.println("CricketCoach : inside no arg constructor");
--- src/com/luv2code/springdemo/SetterDemoApp.java
+++ src/com/luv2code/springdemo/SetterDemoApp.java
@@ -6,9 +6,11 @@ public class SetterDemoApp {
 
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		Coach myCoach = context.getBean("MyCricketCoach", Coach.class);
-		System.out.println(myCoach.getDailyWorkout());
-		System.out.println(myCoach.getDailyFortune());
+		CricketCoach cricketCoach = context.getBean("MyCricketCoach", CricketCoach.class);
+		System.out.println(cricketCoach.getDailyWorkout());
+		System.out.println(cricketCoach.getDailyFortune());
+		System.out.println(cricketCoach.getEmailAddress());
+		System.out.println(cricketCoach.getTeam());
 		context.close();
 	}
 
